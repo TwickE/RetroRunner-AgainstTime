b535649452df7ce77ab0c21045672408bdd75db3
Frederico Flores Moita da Silva <118013263+TwickE@users.noreply.github.com>
2023-12-13 22:52:59 +0000

    Added the game

---
+++ Retro Runner - Against Time/Assets/Scripts/ItemCollectorPlayer1.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollectorPlayer1 : MonoBehaviour
+{
+    private int coinCount = 0; //Number of coins the plyer1 has
+
+    [SerializeField] private Text coinsText;
+
+    [SerializeField] private AudioSource collectionSoundEffect;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Coin"))
+        {
+            collectionSoundEffect.Play(); //Plays the collection sound effect
+            Destroy(collision.gameObject); //Destroys the coin
+            coinCount++; //Adds 1 to the coinCount
+            coinsText.text = coinCount.ToString(); //Updates the text
+        }
+    }
+}
---
+++ Retro Runner - Against Time/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    private bool isMuted = false;
+
+    [SerializeField] private Button targetButton;
+    private Image buttonImage;
+    [SerializeField] private Sprite image1;
+    [SerializeField] private Sprite image2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonImage = targetButton.image; // Get the Image component of the button
+
+        if(AudioListener.volume == 1)
+        {
+            isMuted = false;
+            buttonImage.sprite = image1;
+        }
+        else 
+        {
+            isMuted = true;
+            buttonImage.sprite = image2;
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void ToggleSound()
+    {
+        isMuted = !isMuted;
+
+        if(isMuted)
+        {
+            AudioListener.volume = 0;
+            buttonImage.sprite = image2;
+        }
+        else
+        {
+            AudioListener.volume = 1;
+            buttonImage.sprite = image1;
+        }
+    }
+}
