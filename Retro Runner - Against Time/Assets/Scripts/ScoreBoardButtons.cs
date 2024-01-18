dae8238cdca1fb4a376f0c45d7e853a88ae0cdda
Frederico Flores Moita da Silva <118013263+TwickE@users.noreply.github.com>
2024-01-18 14:41:22 +0000

    Update to the the text in the score board scene

--- Retro Runner - Against Time/Assets/Scripts/ScoreBoardButtons.cs
+++ Retro Runner - Against Time/Assets/Scripts/ScoreBoardButtons.cs
@@ -2,14 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ScoreBoardButtons : MonoBehaviour
 {
     private int previousLevelIndex;
+
+    [SerializeField] private Text nextLevelButtonText;
+
     // Start is called before the first frame update
     void Start()
     {
         previousLevelIndex = PlayerPrefs.GetInt("PreviousLevelIndex"); // Get the previous level index
+        if(previousLevelIndex == 3)
+        {
+            nextLevelButtonText.text = "End Screen"; // Change the text of the next level button to "End Screen" if the previous level was the last level
+        }
     }
     public void NextLevel() //Load the next level
     {
